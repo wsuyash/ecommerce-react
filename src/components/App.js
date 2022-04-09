b0bbc1a1462cc834db495bb97697547f20dcab90
Suyash Wagh <suyashcr07@gmail.com>
2022-04-09 15:47:16 +0530

    Try fix for deployment

--- src/components/App.js
+++ src/components/App.js
@@ -6,49 +6,13 @@ import Products from "./Products";
 import AddProduct from "./AddProduct";
 import Cart from "./Cart";
 import ProductDetail from "./ProductDetail";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
-import { removeSort, setProducts, sortProducts } from "../features/products/productsSlice";
 
 const App = () => {
-	const products = useSelector((state) => state.products.allProducts);
-	const dispatch = useDispatch();
-	const [sort, setSort] = useState(() => false);
-
-
-	const handleSort = () => {
-		setSort(() => true)
-		dispatch(sortProducts());
-	}
-
-	const handleRemoveSort = () => {
-		setSort(() => false)
-		dispatch(removeSort());
-	}
-
-	useEffect(() => {
-		if (products.length < 1) {
-			const getProducts = async () => {
-				try {
-					const response = await fetch('https://my-json-server.typicode.com/wsuyash/fake-db/products');
-					const data = await response.json();
-
-					dispatch(setProducts(data));
-				} catch (error) {
-					toast.error(error.message);
-				}
-			}
-
-			getProducts();
-		}
-	}, [dispatch, products.length]);
-
 	return (
 		<div className="App p-2">
 			<Navbar />
 			<Routes>
-			<Route path="/" element={<Products products={products} handleSort={handleSort} handleRemoveSort={handleRemoveSort} sort={sort} />} />
+				<Route path="ecommerce-react" element={<Products />} />
 				<Route path="add" element={<AddProduct />} />
 				<Route path="cart" element={<Cart />} />
 				<Route path="products/:productId" element={<ProductDetail />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,15 @@ const Navbar = () => {
 		<div className="Navbar flex justify-center md:justify-between items-center gap-2 py-4 px-2 md:p-6">
 			<div className="nav-left flex justify-between items-center gap-4">
 				<div className="w-16 h-16">
-					<Link to="/"><img src="/images/logo.png" alt="logo" /></Link>
+					<Link to="ecommerce-react"><img src="https://cdn-icons-png.flaticon.com/512/1162/1162499.png" alt="logo" /></Link>
 				</div>
-				<Link className="text-blue-400 font-bold hover:text-blue-500" to="/">Products</Link>
+				<Link className="text-blue-400 font-bold hover:text-blue-500" to="ecommerce-react">Products</Link>
 				<Link className="text-blue-400 font-bold hover:text-blue-500" to="/add">Add A Product</Link>
 			</div>
 			<div className="nav-right">
 				<Link to="/cart">
 					<div className="relative">
-						<img src="/images/cart.png" alt="cart" className="w-8 h-8" />
+						<img src="https://cdn-icons-png.flaticon.com/512/1170/1170576.png" alt="cart" className="w-8 h-8" />
 						<div className="w-5 h-5 flex justify-center items-center absolute -top-2 -right-3 text-white text-sm font-bold bg-red-500 rounded-full">{items.length}</div>
 					</div>
 				</Link>
