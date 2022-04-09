ec5f8333c9ed7c0d86b0be15231620e0aecd4286
Suyash Wagh <suyashcr07@gmail.com>
2022-04-09 19:18:29 +0530

    Fix no image error

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -31,7 +31,8 @@ const AddProduct = () => {
 					name,
 					description,
 					price,
-					rating
+					rating,
+					image: "https://cdn-icons.flaticon.com/png/512/3586/premium/3586753.png?token=exp=1649502251~hmac=9b699b3352284d818a5996e3ec795f66"
 				}),
 				headers: {
 					'Content-type': 'application/json; charset=UTF-8',
