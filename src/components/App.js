e6bd12de53f82f8c7d6be0428457f3ade405b59e
Suyash Wagh <suyashcr07@gmail.com>
2022-04-11 00:10:55 +0530

    Try fix paths

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,10 @@ const App = () => {
 		<div className="App p-2">
 			<Navbar />
 			<Routes>
-				<Route path="ecommerce-react" element={<Products />} />
-				<Route path="ecommerce-react/add" element={<AddProduct />} />
-				<Route path="ecommerce-react/cart" element={<Cart />} />
-				<Route path="ecommerce-react/products/:productId" element={<ProductDetail />} />
+				<Route path="/ecommerce-react" element={<Products />} />
+				<Route path="/ecommerce-react/add" element={<AddProduct />} />
+				<Route path="/ecommerce-react/cart" element={<Cart />} />
+				<Route path="/ecommerce-react/products/:productId" element={<ProductDetail />} />
 			</Routes>
 			<ToastContainer autoClose={500} position="top-center" theme="dark" />
 		</div>
